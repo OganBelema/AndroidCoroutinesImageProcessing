2f2840996320146dc0824cc605c9904ba02cc41c
Oganbelema <oganbelema@gmail.com>
2022-08-23 22:30:42 +0100

    made image url a constant

--- app/src/main/java/com/devtides/imageprocessingcoroutines/Constants.kt
+++ app/src/main/java/com/devtides/imageprocessingcoroutines/Constants.kt
@@ -1,3 +1,3 @@
 package com.devtides.imageprocessingcoroutines
 
-val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+const val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
